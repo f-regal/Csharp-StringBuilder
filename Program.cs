3335ddcb19eec6ca3895f45e50e3bef058c6e3fb
Faysal Regal <faysalregal@gmail.com>
2022-08-10 19:55:20 +0100

    Created & Manipulated Strings via StringBuilder

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+namespace CsharpFundamentals
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder("Hello World");
+
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10);
+
+            builder.Replace('-', '+');
+
+            builder.Remove(0, 10);
+
+            builder.Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First Char: " + builder[0]);
+
+
+        }
+    }
+}
